ba04cc59cdae006b1b658c6c309401298dcab52c
Kauã Santos Alves <kauasantosalves@outlook.com>
2024-02-02 18:44:22 -0300

    Responsividade e primeiros produtos

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    freeMode: true,
+    spaceBetween: 0,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    });
